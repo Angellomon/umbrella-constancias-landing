2491b67a6d51a568f1482c459810b2e29674da36
Angellomon <angelmtzdiaz@gmail.com>
2021-04-24 20:29:53 -0500

    :)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
 import { render } from "preact";
 import { App } from "./app";
+import { Router, Route } from "preact-router";
 import "react-circular-progressbar/dist/styles.css";
 import "./index.css";
 
-render(<App />, document.getElementById("app")!);
+render(
+  <Router>
+    <Route path="/" component={App} />
+  </Router>,
+  document.getElementById("app")!
+);
