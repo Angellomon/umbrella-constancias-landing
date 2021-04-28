459008605ce96b9cb7619f90493179ccb4fce6d6
Angellomon <angelmtzdiaz@gmail.com>
2021-04-28 08:25:27 -0500

    :)

--- src/components/Mensaje.tsx
+++ src/components/Mensaje.tsx
@@ -6,7 +6,7 @@ const Mensaje: FC = () => {
       <div class="flex flex-col">
         <p class="text-justify font-light text-white text-xl xl:text-2xl my-3 lg:mt-10 leading-tight">
           Recuerda que para descargar tu constancia, es necesario que hayas
-          cumplido con los requerimientos mínimos de asistencia del{" "}
+          cumplido con el requerimiento mínimo de asistencia del{" "}
           <strong class="text-white text-2xl font-bold">55 %</strong> del
           tiempo. En caso que no lo hayas cubierto, no podrás descargar la
           constancia.
