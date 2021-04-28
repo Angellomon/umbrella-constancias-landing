7a54f79ac4deae07393d1d2c2d68fb47866cc63b
Angellomon <angelmtzdiaz@gmail.com>
2021-04-28 08:21:15 -0500

    :)

--- src/components/Mensaje.tsx
+++ src/components/Mensaje.tsx
@@ -2,16 +2,13 @@ import { FunctionalComponent as FC } from "preact";
 
 const Mensaje: FC = () => {
   return (
-    <article class="flex flex-row mx-8 md:mx-32 my-8">
+    <article class="flex flex-row mx-8 lg:mx-80 md:mx-32 my-8">
       <div class="flex flex-col">
-        <p class="font-light text-white text-xl xl:text-2xl my-3 lg:mt-10 leading-tight">
+        <p class="text-justify font-light text-white text-xl xl:text-2xl my-3 lg:mt-10 leading-tight">
           Recuerda que para descargar tu constancia, es necesario que hayas
           cumplido con los requerimientos mínimos de asistencia del{" "}
           <strong class="text-white text-2xl font-bold">55 %</strong>
-          .<br class="my-3" />
-        </p>
-        <p class="font-light text-white text-xl xl:text-2xl leading-tight">
-          En caso de que no lo hayas cubierto, no podrás descargar tu
+          del tiempo. En caso que no lo hayas cubierto, no podrás descargar la
           constancia.
         </p>
       </div>
